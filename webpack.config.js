b0fcb94409cb72c0548c08254739041d36441da0
Adrián P. Blunier <adrian@numax.org>
2024-07-17 17:18:59 +0200

    Testing github actions env var to fix fonts issue in github pages

--- webpack.config.js
+++ webpack.config.js
@@ -4,14 +4,22 @@ if (!Encore.isRuntimeEnvironmentConfigured()) {
   Encore.configureRuntimeEnvironment(process.env.NODE_ENV || 'dev')
 }
 
+let publicPath = '/'
+if (process.env.PUBLIC_PATH) {
+  publicPath = process.env.PUBLIC_PATH
+}
+
 Encore
   // directory where compiled assets will be stored
   .setOutputPath('dist/')
   // public path used by the web server to access the output path
-  .setPublicPath('/').
-  disableSingleRuntimeChunk().
-  cleanupOutputBeforeBuild().
-  enableSourceMaps(!Encore.isProduction())
+  .setPublicPath(publicPath)
+  //
+  .disableSingleRuntimeChunk()
+  //
+  .cleanupOutputBeforeBuild()
+  //
+  .enableSourceMaps(!Encore.isProduction())
 
   // enable Sass/SCSS support
   .enableSassLoader()
@@ -19,8 +27,10 @@ Encore
   .enablePostCssLoader()
 
   // add entry points
-  .addStyleEntry('css/style', './src/scss/style.scss').
-  addStyleEntry('css/fonts', './src/scss/fonts.scss').
-  addStyleEntry('css/icons', './src/scss/icons.scss')
+  .addStyleEntry('css/style', './src/scss/style.scss')
+  //
+  .addStyleEntry('css/fonts', './src/scss/fonts.scss')
+  //
+  .addStyleEntry('css/icons', './src/scss/icons.scss')
 
 module.exports = Encore.getWebpackConfig()
