015df5eff45bbf846e6351f4b7606e0d9642ab2d
Adrián P. Blunier <adrian@numax.org>
2025-03-28 17:20:04 +0100

    1.0.0 beta 3

--- stories/SiteHeader.stories.ts
+++ stories/SiteHeader.stories.ts
@@ -1,26 +1,61 @@
-import type { Meta, StoryObj } from '@storybook/html'
+import type {Meta, StoryObj} from '@storybook/html'
 
 type SiteHeaderArgs = {
-  label: string;
+    brand: string;
 };
 
 const meta: Meta<SiteHeaderArgs> = {
-  title: 'Organisms/Site Header',
-  tags: ['autodocs'],
-  render: (args) => {
-    const siteHeader = document.createElement('header')
-    siteHeader.innerText = args.label
+    title: 'Organisms/Site Header',
+    tags: ['autodocs'],
+    render: (args) => {
+        const siteHeader = document.createElement('header')
+        const brandLink = document.createElement('a')
+        const navToggle = document.createElement('button')
+        const nav = document.createElement('nav')
+        const langSwitcher = document.createElement('ul')
+        const mainMenu = document.createElement('ul')
 
-    siteHeader.className = ['org-site-header'].join(' ')
+        brandLink.innerText = args.brand
+        brandLink.className = 'text-xl font-bold'
 
-    return siteHeader
-  },
-  argTypes: {
-    label: { control: 'text' },
-  },
-  args: {
-    label: 'SiteHeader',
-  },
+        navToggle.className = 'site-header-nav-toggle'
+        navToggle.ariaLabel = 'Toggle navigation'
+        navToggle.innerHTML = `
+            <i class="fa-solid fa-bars" aria-hidden="true"></i>
+        `
+
+        siteHeader.className = ['org-site-header'].join(' ')
+        nav.className = 'site-header-nav'
+        mainMenu.className = 'site-header-main-menu'
+
+        langSwitcher.className = 'ml-lang-switcher'
+        langSwitcher.innerHTML = `
+            <li><a href="#">GL</a></li>
+            <li><a href="#">ES</a></li>
+            <li><a href="#">EN</a></li>
+        `
+
+        mainMenu.innerHTML = `
+            <li><a href="#">Sobre nós</a></li>
+            <li><a href="#">Proxectos</a></li>
+            <li><a href="#">Contacto</a></li>
+        `
+
+        nav.appendChild(langSwitcher)
+        nav.appendChild(mainMenu)
+
+        siteHeader.appendChild(brandLink)
+        siteHeader.appendChild(navToggle)
+        siteHeader.appendChild(nav)
+
+        return siteHeader
+    },
+    argTypes: {
+        brand: {control: 'text'},
+    },
+    args: {
+        brand: 'Laboratorio NUMAX',
+    },
 }
 
 export default meta
@@ -28,7 +63,7 @@ export default meta
 type Story = StoryObj<SiteHeaderArgs>;
 
 export const SiteHeaderStories: Story = {
-  args: {
-    label: 'SiteHeader',
-  },
+    args: {
+        brand: 'Laboratorio NUMAX',
+    },
 }
